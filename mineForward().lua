ec700a043a028bf339d1dfd9c5e4ebb6ddc8ccbd
OryZenDev <82176616+OryZenDev@users.noreply.github.com>
2022-11-06 15:50:51 -0500

    Update and rename mineForward().lua to mineForward.lua

--- mineForward().lua
+++
@@ -1,9 +0,0 @@
-local args = {...}
-
-local x = 0
-repeat 
-	x = x + 1 
-	turtle.dig() 
-	turtle.forward() 
-	turtle.digUp() 
-until x >= tonumber(args[1])
---
+++ mineForward.lua
@@ -0,0 +1,50 @@
+local args = {...}
+
+local computerName = os.getComputerLabel()
+local urgentCareNeeded = false
+
+function tunnel()
+	turtle.dig()
+	turtle.forward()
+	turtle.digUp()
+end
+
+function findCoal()
+	local slotNum = 0
+	repeat
+		slotNum += 1
+	until slotNum >= 16 or turtle.getItemDetail(slotNum).name == "minecraft:coal"
+	
+	return slotNum
+	
+end
+
+function fuel()
+	local fuelAmount = turtle.getFuelLevel()
+	os.setComputerLabel("Fuel: "..tostring(fuelAmount))
+	if fuelAmount < 20 then
+		local slotNum = findCoal()
+		if turtle.getItemDetail(slotNum).name == "minecraft:coal" then
+			turtle.refuel(turtle.getItemDetail(slotNum).count)
+		else
+			os.setComputerLabel("LOW FUEL!")
+			urgentCareNeeded = true
+		end
+		
+	end
+end
+
+
+
+--// Loop //--
+
+fuel()
+
+local x = 0
+if tonumber(args[1]) == -1 then x = -999999 end
+repeat 
+	x += 1
+	tunnel()
+	fuel()
+	if urgentCareNeeded == true then break end
+until x >= tonumber(args[1])
