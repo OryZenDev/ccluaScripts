ce8d87cb451db59a679fbc88b1fc984dabc7ce1c
OryZenDev <82176616+OryZenDev@users.noreply.github.com>
2022-11-06 14:53:30 -0500

    Update mineForward().lua

--- mineForward().lua
+++ mineForward().lua
@@ -1,9 +1,9 @@
-function mineForward(num) 
-  local x = 0
-  repeat 
-    x = x + 1 
-    turtle.dig() 
-    turtle.forward() 
-    turtle.digUp() 
-  until x >= num 
-end
+local args = {...}
+
+local x = 0
+repeat 
+	x = x + 1 
+	turtle.dig() 
+	turtle.forward() 
+	turtle.digUp() 
+until x >= tonumber(args[1])
