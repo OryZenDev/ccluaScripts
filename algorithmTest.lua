b22601a17cea1b66475011d7f0bc992ea1bd16ff
OryZenDev <82176616+OryZenDev@users.noreply.github.com>
2023-01-24 18:24:02 -0500

    Update algorithmTest.lua

--- algorithmTest.lua
+++ algorithmTest.lua
@@ -1,4 +1,6 @@
-function pos(base, mult)
+local args = {...}
+
+function pos(args[1], args[2])
 	workspace.TestingBox.Position = base.Position + Vector3.new(base.Size.X/2.5, 4, base.Size.Z/2.5)
 end
 
